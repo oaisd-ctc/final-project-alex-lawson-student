eaf81b05c77300194734852d7d7f19ca6da3e2ce
alex-lawson-student <allacd9@student.oaisd.org>
2026-04-30 14:03:23 -0400

    next level works 👽

--- Assets/Script/Level.cs
+++ Assets/Script/Level.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,7 +13,7 @@ public class Level : MonoBehaviour
     [SerializeField] SkillCheck scs;
     public MonoBehaviour it, em, sc, pr;
     public GameObject LE, pl;
-    public TextMeshProUGUI WT, Cmax, HP;
+    public TextMeshProUGUI WT, Cmax, HP, HPN, CmaxN, info;
     public Animator animWT;
     public Animator animE;
     public float LvlTime = 25, MaxSpeed = 10, mag = 5, HpT = 0f;
@@ -21,7 +22,11 @@ public class Level : MonoBehaviour
 
     private void Start()
     {
-
+        HPN.text = "";
+        CmaxN.text = "";
+        HP.text = "";
+        Cmax.text = "";
+        info.text = "";
     }
 
     // Update is called once per frame
@@ -29,6 +34,10 @@ public class Level : MonoBehaviour
     {
         WT.text = (LvlTime-LvlTimeP).ToString("F2");
 
+        if (Input.GetKeyDown(KeyCode.Escape) && end == false)
+        {
+            SceneManager.LoadScene(0);
+        }
 
         if (LvlTimeP <= LvlTime && LvlTime != 0)
         {
@@ -53,6 +62,9 @@ public class Level : MonoBehaviour
                 LvlTime = 0;
             
                 pl.SetActive(false);
+                HPN.text = "People Talked";
+                CmaxN.text = "Best color";
+                info.text = "[Space for next Level] [Esc to main menu]";
                 HP.text = scs.HP.ToString("") + " / " + HpT;
 
                 float[] myArray = { scs.HCg, scs.HCy, scs.HCo, scs.HCr };
@@ -79,13 +91,30 @@ public class Level : MonoBehaviour
                 animE.SetBool("EU", true);
                 Cmax.text = "";
                 HP.text = "";
+
                 if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
                 {
-                    SceneManager.LoadScene(0);
+                    int currentIndex = SceneManager.GetActiveScene().buildIndex;
+
+                    if (currentIndex + 1 < SceneManager.sceneCountInBuildSettings)
+                    {
+                        SceneManager.LoadScene(currentIndex + 1);
+                    }
+                    else
+                    {
+                        Cmax.text = "";
+                        HP.text = "";
+                        HPN.text = "";
+                        info.text = "";
+                        CmaxN.text = "";
+                        SceneManager.LoadScene(0);
+
+                    }
+
                     LE.SetActive(false);
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape) && end == true)
             {
                 animE.SetBool("ED", false);
                 animE.SetBool("EU", true);
@@ -93,7 +122,7 @@ public class Level : MonoBehaviour
                 HP.text = "";
                 if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
                 {
-                    SceneManager.LoadScene(1);
+                    SceneManager.LoadScene(0);
                     LE.SetActive(false);
                 }
             }
