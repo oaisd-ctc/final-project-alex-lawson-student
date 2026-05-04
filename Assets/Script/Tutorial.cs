eff576de3e069e64151f8c3db2e9cbae8ea18212
alex-lawson-student <allacd9@student.oaisd.org>
2026-05-04 14:45:06 -0400

    stuff

--- Assets/Script/Tutorial.cs
+++ Assets/Script/Tutorial.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Threading;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text uiText;          // Assign in Inspector
+    public string[] messages = new string[] 
+    { "Welcome Jesse, to Yappa",
+    "I am Mr. White",
+    "Right now",
+    "We need to Yap",
+    "But first, we need to move",
+    "Use WASD to move",
+    "Now Jesse, listen close",
+    "We need to find an NPC",
+    "When by NPC",
+    "We need to Yap",
+    "Press SPACE to Yap",
+    "Aim for the green bar",
+    "Or else Jesse",
+    "You’ll get embarrassed",
+    "If embarrassed",
+    "You’ll move slower",
+    "And the pin moves faster",
+    "And remember Jesse",
+    "You’re on a timer",
+    "Now, go get ‘em Steve"};
+    public float interval = 5f;
+    private int currentIndex = 0;
+
     void Start()
     {
-        
+        if (uiText == null)
+        {
+            Debug.LogError("uiText is not assigned!");
+            return;
+        }
+
+        if (messages.Length > 0)
+        {
+            uiText.text = messages[currentIndex];
+            InvokeRepeating(nameof(ChangeText), interval, interval);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ChangeText()
     {
-        
+        currentIndex = (currentIndex + 1) % messages.Length;
+        uiText.text = messages[currentIndex];
     }
 }
