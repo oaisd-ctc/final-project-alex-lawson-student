fa467ddcd34403ad04c3beedacf2983a55d8886d
alex-lawson-student <allacd9@student.oaisd.org>
2026-05-06 14:14:07 -0400

    fix and made Tutorial

--- Assets/Script/Level.cs
+++ Assets/Script/Level.cs
@@ -16,12 +16,23 @@ public class Level : MonoBehaviour
     public TextMeshProUGUI WT, Cmax, HP, HPN, CmaxN, info;
     public Animator animWT;
     public Animator animE;
-    public float LvlTime = 25, MaxSpeed = 10, mag = 5, HpT = 0f;
+    public float LvlTime = 25, MaxSpeed = 10, mag = 5, HpT = 0, Totorlvl = 0;
     public bool end = false;
     float LvlTimeP;
 
     private void Start()
     {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentIndex > 0)
+        {
+            Totorlvl = 5;
+        }
+        else
+        {
+            Totorlvl = 0;
+        }
+
         HPN.text = "";
         CmaxN.text = "";
         HP.text = "";
@@ -38,95 +49,96 @@ public class Level : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
-
-        if (LvlTimeP <= LvlTime && LvlTime != 0)
+        if (Totorlvl == 5)
         {
-            LvlTimeP += Time.deltaTime;
-
-            animWT.speed = (MaxSpeed * math.pow(LvlTimeP, mag)) / math.pow(LvlTime, mag) + 0.2f;
-            LE.SetActive(false);
-        }
-        else
-        {
-            LE.SetActive(true);
-            animE.SetBool("EU", false);
-            animE.SetBool("ED", true);
-
-
-            if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f && end == false)
+            if (LvlTimeP <= LvlTime && LvlTime != 0)
             {
-                animE.SetBool("ED", false);
-                end = true;
-                animWT.speed = 0;
-                LvlTimeP = 0;
-                LvlTime = 0;
-            
-                pl.SetActive(false);
-                HPN.text = "People Talked";
-                CmaxN.text = "Best color";
-                info.text = "Space for next Level        Esc to main menu";
-                HP.text = scs.HP.ToString("") + " / " + HpT;
+                LvlTimeP += Time.deltaTime;
 
-                float[] myArray = { scs.HCg, scs.HCy, scs.HCo, scs.HCr };
-
-                switch (System.Array.IndexOf(myArray, Mathf.Max(myArray)))
-                {
-                    case 0:
-                        Cmax.text = "Green";
-                        break;
-                    case 1:
-                        Cmax.text = "Yellow";
-                        break;
-                    case 2:
-                        Cmax.text = "Orange";
-                        break;
-                    case 3:
-                        Cmax.text = "Red";
-                        break;
-                }
+                animWT.speed = (MaxSpeed * math.pow(LvlTimeP, mag)) / math.pow(LvlTime, mag) + 0.2f;
+                LE.SetActive(false);
             }
-            if(Input.GetKeyDown(KeyCode.Space))
+            else
             {
-                animE.SetBool("ED", false);
-                animE.SetBool("EU", true);
-                Cmax.text = "";
-                HP.text = "";
+                LE.SetActive(true);
+                animE.SetBool("EU", false);
+                animE.SetBool("ED", true);
 
-                if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+
+                if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f && end == false)
                 {
-                    int currentIndex = SceneManager.GetActiveScene().buildIndex;
+                    animE.SetBool("ED", false);
+                    end = true;
+                    animWT.speed = 0;
+                    LvlTimeP = 0;
+                    LvlTime = 0;
 
-                    if (currentIndex + 1 < SceneManager.sceneCountInBuildSettings)
+                    pl.SetActive(false);
+                    HPN.text = "People Talked";
+                    CmaxN.text = "Best color";
+                    info.text = "Space for next Level        Esc to main menu";
+                    HP.text = scs.HP.ToString("") + " / " + HpT;
+
+                    float[] myArray = { scs.HCg, scs.HCy, scs.HCo, scs.HCr };
+
+                    switch (System.Array.IndexOf(myArray, Mathf.Max(myArray)))
                     {
-                        SceneManager.LoadScene(currentIndex + 1);
+                        case 0:
+                            Cmax.text = "Green";
+                            break;
+                        case 1:
+                            Cmax.text = "Yellow";
+                            break;
+                        case 2:
+                            Cmax.text = "Orange";
+                            break;
+                        case 3:
+                            Cmax.text = "Red";
+                            break;
                     }
-                    else
+                }
+                if (Input.GetKeyDown(KeyCode.Space) && end == true)
+                {
+                    animE.SetBool("ED", false);
+                    animE.SetBool("EU", true);
+                    Cmax.text = "";
+                    HP.text = "";
+
+                    if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
                     {
-                        Cmax.text = "";
-                        HP.text = "";
-                        HPN.text = "";
-                        info.text = "";
-                        CmaxN.text = "";
+                        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+
+                        if (currentIndex + 1 < SceneManager.sceneCountInBuildSettings)
+                        {
+                            SceneManager.LoadScene(currentIndex + 1);
+                        }
+                        else
+                        {
+                            Cmax.text = "";
+                            HP.text = "";
+                            HPN.text = "";
+                            info.text = "";
+                            CmaxN.text = "";
+                            SceneManager.LoadScene(1);
+
+                        }
+
+                        LE.SetActive(false);
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Escape) && end == true)
+                {
+                    animE.SetBool("ED", false);
+                    animE.SetBool("EU", true);
+                    Cmax.text = "";
+                    HP.text = "";
+                    if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+                    {
                         SceneManager.LoadScene(1);
-
+                        LE.SetActive(false);
                     }
-
-                    LE.SetActive(false);
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Escape) && end == true)
-            {
-                animE.SetBool("ED", false);
-                animE.SetBool("EU", true);
-                Cmax.text = "";
-                HP.text = "";
-                if (animE.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
-                {
-                    SceneManager.LoadScene(1);
-                    LE.SetActive(false);
-                }
-            }
-
         }
     }
 }
--- Assets/Script/Tutorial.cs
+++ Assets/Script/Tutorial.cs
@@ -1,18 +1,146 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Level level;
+    public TextMeshProUGUI Tutortext;
+    private float timetalk = 0;
+    public bool goT = true;
+    public float targetTime = 0;
+    public float targettTime = 5;
+    private void Update()
     {
-        
-    }
+        if(level.end == false)
+        {
+            goT = true;
+            if (goT == true)
+            {
+                timetalk += Time.deltaTime;
+            }
+            print(timetalk);
+            if (level.Totorlvl == 0)
+            {
+                Tutortext.text = "Welcome to Yappa and We need to Yap. B)";
+                targetTime += targettTime;
+                level.Totorlvl = 1.5f;
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (level.Totorlvl == 1.5 && timetalk >= targetTime)
+            {
+                Tutortext.text = "But first, we need to move. 0_0";
+                targetTime += targettTime;
+                level.Totorlvl = 2;
+            }
+
+            else if (level.Totorlvl == 2 && timetalk >= targetTime)
+            {
+                Tutortext.text = "Use WASD to move.";
+                targetTime += targettTime;
+
+                //goT = false;
+                level.Totorlvl = 2.5f;
+            }
+
+            else if (level.Totorlvl == 2.5 && timetalk >= targetTime)
+            {
+                Tutortext.text = "Now listen close. ()_()";
+                targetTime += targettTime;
+
+                level.Totorlvl = 3;
+            }
+            else if (level.Totorlvl == 3 && timetalk >= targetTime)
+            {
+                Tutortext.text = "We need to find an NPC.";
+                targetTime += targettTime;
+
+                //goT = false;
+                level.Totorlvl = 3.1f;
+            }
+            else if (level.Totorlvl == 3.1 && timetalk >= targetTime)
+            {
+                Tutortext.text = "There will be a SKILL CHECK!!!";
+                targetTime += targettTime;
+
+                level.Totorlvl = 3.2f;
+            }
+            else if (level.Totorlvl == 3.2 && timetalk >= targetTime)
+            {
+                Tutortext.text = "Press SPACE to stop the pin. Remember to Aim for the green bar.";
+                targetTime += targettTime;
+
+                level.Totorlvl = 3.3f;
+            }
+            else if (level.Totorlvl == 3.3 && timetalk >= targetTime)
+            {
+                Tutortext.text = "Or else...";
+                targetTime += targettTime;
+
+                level.Totorlvl = 4f;
+            }
+            else if (level.Totorlvl == 4 && timetalk >= targetTime)
+            {
+                Tutortext.text = "You’ll get embarrassed";
+                targetTime += targettTime;
+
+                level.Totorlvl = 4.1f;
+            }
+            else if (level.Totorlvl == 4.1 && timetalk >= targetTime)
+            {
+                Tutortext.text = "If embarrassed";
+                targetTime += targettTime;
+
+                level.Totorlvl = 4.2f;
+            }
+            else if (level.Totorlvl == 4.2 && timetalk >= targetTime)
+            {
+                Tutortext.text = "You’ll move slower";
+                targetTime += targettTime;
+
+                level.Totorlvl = 4.3f;
+            }
+            else if (level.Totorlvl == 4.3 && timetalk >= targetTime)
+            {
+                Tutortext.text = "And the pin moves faster";
+                targetTime += targettTime;
+
+                level.Totorlvl = 4.4f;
+            }
+            else if (level.Totorlvl == 4.4 && timetalk >= targetTime)
+            {
+                Tutortext.text = "And remember...";
+
+                targetTime += targettTime;
+                level.Totorlvl = 4.5f;
+            }
+            else if (level.Totorlvl == 4.5 && timetalk >= targetTime)
+            {
+                Tutortext.text = "You’re on a timer";
+
+                targetTime += targettTime;
+                level.Totorlvl = 4.6f;
+            }
+            else if (level.Totorlvl == 4.6 && timetalk >= targetTime)
+            {
+                Tutortext.text = "Now, go get ‘em Steve";
+
+                targetTime += targettTime;
+                level.Totorlvl = 5f;
+            }
+            else if (level.Totorlvl == 5 && timetalk >= targetTime)
+            {
+                Tutortext.text = "";
+
+                targetTime += targettTime;
+                level.Totorlvl = 5f;
+            }
+        }
+        else
+        {
+            Tutortext.text = "";
+        }
     }
 }
